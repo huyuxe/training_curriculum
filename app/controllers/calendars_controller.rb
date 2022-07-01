5e02a24031c6b1334f84afdb11dff079b59371ec
huyuxe <107824613+huyuxe@users.noreply.github.com>
2022-07-01 23:24:23 +0900

    issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -4,6 +4,7 @@ class CalendarsController < ApplicationController
   def index
     get_week
     @plan = Plan.new
+
   end
 
   # 予定の保存
@@ -34,7 +35,14 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+
+      wday_num = Date.today.wday + x
+
+      if wday_num >= 7
+        wday_num = wday_num -7
+      end
+
+      days = {month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
     end
 
